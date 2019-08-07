3a0ba9bd6655208a4e5c9141ff2fc35fe1f8a6c9
Pablo Barberá <pablobarbera@gmail.com>
2019-08-06 22:21:36 -0700

    fixes #9

--- pkg/tweetscores/R/format-twitter-date.R
+++ pkg/tweetscores/R/format-twitter-date.R
@@ -13,11 +13,13 @@
 #'
 
 formatTwDate <- function(datestring, format="datetime"){
+    Sys.setlocale("LC_TIME", "English") # change locale settings
     if (format=="datetime"){
         date <- as.POSIXct(datestring, format="%a %b %d %H:%M:%S %z %Y")
     }
     if (format=="date"){
         date <- as.Date(datestring, format="%a %b %d %H:%M:%S %z %Y")
     }
+    Sys.setlocale('LC_TIME',Sys.getlocale('LC_COLLATE')) # return to normal
     return(date)
 }
